ae61e666bf64fe03852989160b75fd08f61c3644
onurhanakbulut <onurhanakbulut1@hotmail.com>
2025-08-07 19:13:49 +0300

    YOLO detection and DeepSORT tracking

--- traffic_tracker/main.py
+++ traffic_tracker/main.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 import cv2
-
+from utils import get_tracker
+import math
 
 # =============================================================================
-# 15 dakikalık sabit kamera görüntüsünde
-# 
-# YOLO ile araçları tespit etmek
+# YOLO ile araçları tespit etmek#####tmm
 # 
 # ROI ile 4 şerit bölgesi tanımlamak
 # 
@@ -19,7 +18,19 @@ import cv2
 
 model = YOLO('yolov8n.pt')
 
+tracker = get_tracker()
+
 cap = cv2.VideoCapture('data/traffic.mp4')
+
+allowed_labels = ['car', 'truck', 'bus', 'motorcycle']
+
+color_map = {
+    'car' : (0, 255, 0),
+    'truck' : (255, 0, 0),
+    'bus' : (0, 0, 255),
+    'motorcycle' : (255, 255, 0)
+    }
+
 
 
 while cap.isOpened():
@@ -31,10 +42,84 @@ while cap.isOpened():
     
     
     results = model(frame)
+
+    #annotated_frame = results[0].plot()
+   
+    detections =  []
+   
+   
+    for box in results[0].boxes:
+        class_id = int(box.cls[0])
+        conf = float(box.conf[0])
+        label = model.names[class_id]
     
-    annotated_frame = results[0].plot()
+        if label not in allowed_labels:
+            continue
+        
+        
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        w, h = x2 - x1, y2 - y1
+        
+        detections.append(([x1, y1, w, h], conf, label))
+        
     
-    cv2.imshow("Araç Tespiti", annotated_frame)
+        
+    tracks = tracker.update_tracks(detections, frame=frame)
+    
+    frame_centers = []
+    
+    for track in tracks:
+        if not track.is_confirmed():
+            continue
+        
+        
+        track_id = track.track_id
+        ltrb = track.to_ltrb()      #left top right bottom
+        x1, y1, x2, y2 = map(int, ltrb)
+        
+        cx = int((x1 + x2) / 2)
+        cy = int((y1 + y2) / 2)
+        
+        
+        too_close = False
+        
+        for (px, py) in frame_centers:
+            if math.hypot(cx - px, cy - py) < 100:
+                too_close = True
+                break
+            
+            
+        if too_close:
+            continue
+        
+        frame_centers.append((cx, cy))
+        
+        
+
+        
+        label = track.get_det_class() or 'Vehicle'
+        
+        if label in ['truck', 'bus']:
+            label = 'heavy'
+        
+        color = color_map.get(label, (255, 255, 255))
+        text = f"{label.upper()} #{track_id}"
+        
+        
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        
+        
+        (tw, th), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
+        cv2.rectangle(frame, (x1, y1 - th - 10), (x1 + tw + 4, y1), color, -1)
+        
+        
+        cv2.putText(frame, text, (x1 + 2, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
+        
+        cv2.circle(frame, (cx, cy), 12, (0, 0, 255), -1)
+        
+  
+    
+    cv2.imshow("YOLO + DEEPSORT", frame)
     
     
     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -42,3 +127,22 @@ while cap.isOpened():
     
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- traffic_tracker/utils.py
+++ traffic_tracker/utils.py
@@ -1,7 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  7 16:25:31 2025
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
-@author: ohabulut
-"""
+
+def get_tracker():
+    
+    
+    return DeepSort(
+        max_age = 20,
+        n_init = 3,
+        nms_max_overlap = 0.7,
+        max_cosine_distance = 0.4,
+        nn_budget = None, 
+        override_track_class = None
+    )
+
 
